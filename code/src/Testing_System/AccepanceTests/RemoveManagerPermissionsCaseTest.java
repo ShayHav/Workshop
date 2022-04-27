792a4c9d115f7c81f433789baf9e233ae8446dfb
arielr77 <arielron@post.bgu.ac.il>
2022-04-27 12:10:00 +0300

    More UseCases Done

---
+++ code/src/Testing_System/AccepanceTests/RemoveManagerPermissionsCaseTest.java
@@ -0,0 +1,164 @@
+package Testing_System.AccepanceTests;
+import Testing_System.Tester;
+import Testing_System.UserGenerator;
+import domain.shop.Shop;
+import domain.shop.ShopManagersPermissions;
+import domain.shop.ShopPermissions;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RemoveManagerPermissionsCaseTest extends Tester{
+
+    private UserGenerator ug;
+    private String[] validUsers;
+    private String[] pws;
+    private String user_1;
+    private String pw_1;
+    private String owner;
+    private String owner_pw;
+    private String manager;
+    private String manager_pw;
+    private int shopID_1;
+    private List<ShopManagersPermissions> ls;
+    private List<ShopManagersPermissions> pToRemove;
+
+
+    @BeforeAll
+    public void SetUp() {
+        ug = new UserGenerator();
+        validUsers = ug.GetValidUsers();
+        pws = ug.GetPW();
+        ug.InitTest();
+        user_1 = validUsers[0];
+        pw_1 = pws[0];
+        Register(user_1, pw_1);
+        Login(user_1, pw_1);
+        shopID_1 = CreateShop(user_1, "TestShop").GetSecondElement();
+        owner = validUsers[1];
+        owner_pw = pws[1];
+        Register(owner,owner_pw);
+        AppointNewShopOwner(shopID_1,owner,user_1);
+        manager = validUsers[2];
+        manager_pw = pws[2];
+        Register(manager,manager_pw);
+        AppointNewShopManager(shopID_1,manager,user_1);
+        List<ShopManagersPermissions> ls = new ArrayList<ShopManagersPermissions>();
+        ls.add(ShopManagersPermissions.RemoveProductFromInventory);
+        ls.add(ShopManagersPermissions.AddProductToInventory);
+
+    }
+
+
+    @AfterEach
+    public void Demote() {
+        Logout(owner);
+        Logout(manager);
+        String[] arr = new String[ug.getNumOfUser() - 2];
+        for (int i = 3; i < ug.getNumOfUser(); i++)
+            arr[i - 2] = validUsers[i];
+        DeleteUserTest(arr);
+    }
+
+    @AfterAll
+    public void CleanUp() {
+        DeleteUserTest(validUsers);
+        ug.DeleteAdmin();
+    }
+
+    @BeforeEach
+    public void AddPermissions()
+    {
+        pToRemove = new ArrayList<ShopManagersPermissions>();
+        AddShopMangerPermissions(shopID_1,ls,manager,user_1);
+    }
+
+    @Test
+    public void RemovePermissionsSuccessTest()
+    {
+        Login(manager,manager_pw);
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        assertTrue(RemoveShopManagerPermissions(shopID_1,pToRemove, manager,user_1).GetFirstElement());
+    }
+
+    @Test
+    public void RemoveMultiplePermissionsSuccessTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        pToRemove.add(ShopManagersPermissions.RemoveProductFromInventory);
+        assertTrue(RemoveShopManagerPermissions(shopID_1,pToRemove, manager,user_1).GetFirstElement());
+
+    }
+
+    @Test
+    public void TwoStepRemoveSuccessTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        assertTrue(RemoveShopManagerPermissions(shopID_1,pToRemove, manager,user_1).GetFirstElement());
+        pToRemove.add(ShopManagersPermissions.RemoveProductFromInventory);
+        assertTrue(RemoveShopManagerPermissions(shopID_1,pToRemove, manager,user_1).GetFirstElement());
+
+    }
+
+    @Test
+    public void AppointedOwnerRemovesPermissionTest()
+    {
+        Login(owner,owner_pw);
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        assertTrue(RemoveShopManagerPermissions(shopID_1,ls, manager,owner).GetFirstElement());
+    }
+
+    @Test
+    public void ManagerNotLoggedGoodTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        assertTrue(RemoveShopManagerPermissions(shopID_1,pToRemove, manager,user_1).GetFirstElement());
+    }
+
+    @Test
+    public void NotLoggedInCaseFailTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        assertFalse(RemoveShopManagerPermissions(shopID_1,pToRemove, manager,owner).GetFirstElement());
+    }
+
+    @Test
+    public void RemoveToNoManagerTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        Register(validUsers[3],pws[3]);
+        assertFalse(RemoveShopManagerPermissions(shopID_1,pToRemove, validUsers[3], user_1).GetFirstElement());
+        assertFalse(RemoveShopManagerPermissions(shopID_1,pToRemove, owner,user_1).GetFirstElement());
+
+    }
+
+    @Test
+    public void RemoveNotRegisteredTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        assertFalse(RemoveShopManagerPermissions(shopID_1,pToRemove, validUsers[3], user_1).GetFirstElement());
+    }
+
+    @Test
+    public void BadInputsTest()
+    {
+        pToRemove.add(ShopManagersPermissions.AddProductToInventory);
+        List<ShopManagersPermissions> ls_2 = new ArrayList<ShopManagersPermissions>();
+        Register(validUsers[3],pws[3]);
+        AppointNewShopManager(shopID_1,validUsers[2],user_1);
+        assertFalse(RemoveShopManagerPermissions(shopID_1+1,pToRemove, validUsers[3], user_1).GetFirstElement());
+        assertFalse(RemoveShopManagerPermissions(-1,pToRemove, validUsers[3], user_1).GetFirstElement());
+        assertFalse(RemoveShopManagerPermissions(shopID_1,ls_2, validUsers[3], user_1).GetFirstElement());
+        assertFalse(RemoveShopManagerPermissions(shopID_1,null, validUsers[3], user_1).GetFirstElement());
+        assertFalse(RemoveShopManagerPermissions(shopID_1,pToRemove, null, user_1).GetFirstElement());
+        assertFalse(RemoveShopManagerPermissions(shopID_1,pToRemove, validUsers[3], null).GetFirstElement());
+
+
+    }
+
+
+}
---
+++ code/src/Testing_System/AccepanceTests/RemoveProductFromShopCaseTest.java
@@ -0,0 +1,203 @@
+package Testing_System.AccepanceTests;
+import Testing_System.Tester;
+import Testing_System.UserGenerator;
+import domain.shop.ShopManagersPermissions;
+import domain.shop.ShopPermissions;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class RemoveProductFromShopCaseTest extends Tester {
+
+    private UserGenerator ug;
+    private String[] validUsers;
+    private String[] pws;
+    private String user_1;
+    private String pw_1;
+    private String user_2;
+    private String pw_2;
+    private int shopID_1;
+    private int shopID_2;
+    private int shopID_3;
+    private String pName_1;
+    private String pDis_1;
+    private String pCat_1;
+    private double price_1;
+    private int amountToAdd_1;
+    private String pName_2;
+    private String pDis_2;
+    private String pCat_2;
+    private double price_2;
+    private int amountToAdd_2;
+    private String Guest_Id;
+    private int pID_1;
+    private int pID_2;
+    private int pID_3;
+    private int pID_4;
+    private int pID_5;
+    private int pID_6;
+
+
+
+
+    @BeforeAll
+    public void SetUp()
+    {
+        ug = new UserGenerator();
+        validUsers = ug.GetValidUsers();
+        pws = ug.GetPW();
+        ug.InitTest();
+        user_1 = validUsers[0];
+        user_2 = validUsers[1];
+        pw_1 = pws[0];
+        pw_2 = pws[1];
+        Guest_Id = EnterMarket().GetSecondElement();
+        pName_1 = "Durex";
+        pDis_1 = "Protection rubber item. Single item.";
+        pCat_1 = "Sex";
+        price_1 = 4.6;
+        amountToAdd_1 = 1000;
+        pName_2 = "Vibrator";
+        pDis_2 = "Man genitalia sex toy.";
+        pCat_2 = "Sex";
+        price_2 = 99.9;
+        amountToAdd_2 = 50;
+    }
+
+    @BeforeEach
+    public void SetShops()
+    {
+        Register(user_1, pw_1);
+        Login(user_1,pw_1);
+        Register(user_2, pw_2);
+        Login(user_2,pw_2);
+        shopID_1 = CreateShop(user_1, "TestShop_1").GetSecondElement();
+        shopID_2 = CreateShop(user_2, "TestShop_2").GetSecondElement();
+        shopID_3 = CreateShop(user_2, "TestShop_3").GetSecondElement();
+        pID_1 = AddProductToShopInventory(pName_1,pDis_1,pCat_1,price_1, amountToAdd_1, user_1, shopID_1).GetSecondElement();
+        pID_2 = AddProductToShopInventory(pName_2,pDis_2,pCat_2,price_2, amountToAdd_2, user_1, shopID_1).GetSecondElement();
+
+        pID_3 = AddProductToShopInventory(pName_1,pDis_1,pCat_1,price_1, amountToAdd_1, user_2, shopID_2).GetSecondElement();
+        pID_4 = AddProductToShopInventory(pName_2,pDis_2,pCat_2,price_2, amountToAdd_2, user_2, shopID_2).GetSecondElement();
+
+        pID_5 = AddProductToShopInventory(pName_1,pDis_1,pCat_1,price_1, amountToAdd_1, user_2, shopID_3).GetSecondElement();
+        pID_6 = AddProductToShopInventory(pName_2,pDis_2,pCat_2,price_2, amountToAdd_2, user_2, shopID_3).GetSecondElement();
+
+    }
+
+    @AfterEach
+    public void Reset()
+    {
+        DeleteUserTest(validUsers);
+    }
+
+    @AfterAll
+    public void CleanUp()
+    {
+        LeaveMarket();
+        ug.DeleteAdmin();
+    }
+
+    @Test
+    public void GoodRemovalTest() {
+        assertTrue(RemoveProductFromShopInventory(pID_1, user_1, shopID_1).GetFirstElement());
+        assertTrue(RemoveProductFromShopInventory(pID_2, user_1, shopID_1).GetFirstElement());
+
+        assertTrue(RemoveProductFromShopInventory(pID_3, user_2, shopID_2).GetFirstElement());
+        assertTrue(RemoveProductFromShopInventory(pID_4, user_2, shopID_2).GetFirstElement());
+
+    }
+
+    @Test
+    public void SameProductsAndFounderDifferentShopRemovalTest()
+    {
+        assertTrue(RemoveProductFromShopInventory(pID_3, user_2, shopID_2).GetFirstElement());
+        assertTrue(RemoveProductFromShopInventory(pID_4, user_2, shopID_2).GetFirstElement());
+
+        assertTrue(RemoveProductFromShopInventory(pID_5, user_2, shopID_3).GetFirstElement());
+        assertTrue(RemoveProductFromShopInventory(pID_6, user_2, shopID_3).GetFirstElement());
+    }
+
+    @Test
+    public void AppointOwnerRemovalTest()
+    {
+        Register(validUsers[2],pws[2]);
+        Login(validUsers[2],pws[2]);
+        AppointNewShopOwner(shopID_1,validUsers[2],user_1);
+        assertTrue(RemoveProductFromShopInventory(pID_2, validUsers[2], shopID_1).GetFirstElement());
+        assertTrue(RemoveProductFromShopInventory(pID_1, validUsers[2], shopID_1).GetFirstElement());
+    }
+
+    @Test
+    public void AppointManagerInsertTest()
+    {
+        Register(validUsers[2],pws[2]);
+        Login(validUsers[2],pws[2]);
+        AppointNewShopManager(shopID_1,validUsers[2],user_1);
+        ShopManagersPermissions sp = ShopManagersPermissions.RemoveProductFromInventory;
+        List<ShopManagersPermissions> ls = new ArrayList<ShopManagersPermissions>();
+        ls.add(sp);
+        AddShopMangerPermissions(shopID_1,ls,validUsers[2],user_1);
+        assertTrue(RemoveProductFromShopInventory(pID_2, validUsers[2], shopID_1).GetFirstElement());
+    }
+
+    @Test
+    public void NotRegisteredUserTest()
+    {
+        assertFalse(RemoveProductFromShopInventory(pID_2, validUsers[2], shopID_1).GetFirstElement());
+    }
+
+    @Test
+    public void NotLoggedInUserTest()
+    {
+        Logout(user_1);
+        assertFalse(RemoveProductFromShopInventory(pID_2, user_1, shopID_1).GetFirstElement());
+    }
+
+    @Test
+    public void NoPermissionRemovalTest() {
+        Register(validUsers[2], pws[2]);
+        Login(validUsers[2], pws[2]);
+        Register(validUsers[3], pws[3]);
+        Login(validUsers[3], pws[3]);
+        Register(validUsers[ug.getNumOfUser() - 1], pws[ug.getNumOfUser() - 1]);
+        Login(validUsers[ug.getNumOfUser() - 1], pws[ug.getNumOfUser() - 1]);
+        AppointNewShopManager(shopID_2, validUsers[ug.getNumOfUser() - 1], user_2); //appointed, no permissions were given yet
+        AppointNewShopManager(shopID_1, validUsers[2], user_1);
+        ShopManagersPermissions sp = ShopManagersPermissions.RemoveProductFromInventory;
+        List<ShopManagersPermissions> ls = new ArrayList<ShopManagersPermissions>();
+        ls.add(sp);
+        AddShopMangerPermissions(shopID_1, ls, validUsers[2], user_1);
+        AppointNewShopOwner(shopID_1, validUsers[3], user_1);
+
+        assertFalse(RemoveProductFromShopInventory(pID_2, validUsers[ug.getNumOfUser() - 1], shopID_2).GetFirstElement());
+        assertFalse(RemoveProductFromShopInventory(pID_1, validUsers[ug.getNumOfUser() - 1], shopID_2).GetFirstElement());
+
+        assertFalse(RemoveProductFromShopInventory(pID_1, validUsers[2], shopID_2).GetFirstElement());
+        assertFalse(RemoveProductFromShopInventory(pID_2, validUsers[2], shopID_2).GetFirstElement());
+
+        assertFalse(RemoveProductFromShopInventory(pID_2, validUsers[3], shopID_2).GetFirstElement());
+        assertFalse(RemoveProductFromShopInventory(pID_1, validUsers[3], shopID_2).GetFirstElement());
+
+
+    }
+
+    @Test
+    public void ProductAlreadyRemovedTest()
+    {
+        assertTrue(RemoveProductFromShopInventory(pID_1, user_1, shopID_1).GetFirstElement());
+        assertFalse(RemoveProductFromShopInventory(pID_2, user_1, shopID_1).GetFirstElement());
+    }
+
+    @Test
+    public void BadProductInputsTest() {
+        assertFalse(RemoveProductFromShopInventory(-1, user_1, shopID_1).GetFirstElement());
+        assertFalse(RemoveProductFromShopInventory(pID_2, null, shopID_1).GetFirstElement());
+        assertFalse(RemoveProductFromShopInventory(pID_2, user_1, -1).GetFirstElement());
+
+    }
+
+}
