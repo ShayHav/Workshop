40a54fcd11ff50a2860420ef2b79ce01989800d4
Shay <shayhav@post.bgu.ac.il>
2022-05-10 18:10:29 +0300

    added register page and nav bar to the pages

--- code/src/Presentation/Main.java
+++ code/src/Presentation/Main.java
@@ -1,15 +1,20 @@
 package Presentation;
 
+import Presentation.Model.PresentationShop;
 import Service.Services;
 import io.javalin.Javalin;
 import io.javalin.core.JavalinConfig;
 
+import java.util.List;
+
 public class Main {
 
     static final int port = 8080;
-
+    static Services services;
 
     public static void main(String[] args) {
+        services = new Services();
+        services.StartMarket(null,null, "","");
         Javalin app =Javalin.create(JavalinConfig::enableWebjars).start(port);
 
         app.post("/users" , ctx ->{
@@ -17,9 +22,15 @@ public class Main {
         });
 
         app.get("/", ctx ->{
-            Services services = new Services();
-            List<Shop> shops = services.getShops();
+
+            List<PresentationShop> shops = services.getShops();
             ctx.render("index.jte", );
+        });
+
+        app.ws("/user/new", ws ->{
+            ws.onMessage(ctx->{
+                String username = ctx.message();
+            });
         });
 
         app.before(ctx->{
@@ -27,7 +38,7 @@ public class Main {
                 //enter market from servic;
                 ctx.cookie("uid", )
             }
-        })
+        });
 
     }
 
