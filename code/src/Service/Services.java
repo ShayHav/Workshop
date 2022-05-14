27b8b11d760b31595665f4e485c331b6acbf3d74
nitay vitkin <nitayv@post.bgu.ac.il>
2022-05-14 03:03:45 +0300

    document services class

--- code/src/Service/Services.java
+++ code/src/Service/Services.java
@@ -36,6 +36,13 @@ public class Services {
      */
     //General Guest-Visitor
     //Make:nitay InvalidSequenceOperationsExc, BlankDataExc, IncorrectIdentification
+
+    /**
+     * User logged in to the system
+     * @param username - user identifier
+     * @param pw - given password
+     * @return - Response object
+     */
     public ResponseT<PresentationUser> Login(String username, String pw) {
         ResponseT<PresentationUser> output;
         try {
@@ -53,6 +60,13 @@ public class Services {
         }
     }
     //Make:nitay
+
+    /**
+     * User registered to the market
+     * @param username - given user identifier
+     * @param pw - given user password
+     * @return
+     */
     public Response Register(String username, String pw) {
         try {
             if(marketSystem.register(username, pw))
@@ -65,6 +79,10 @@ public class Services {
         return new Response();
     }
     //Make:nitay
+    /**
+     * User enters the system
+     * @return response object
+     */
     public ResponseT<PresentationUser> EnterMarket() { //
         User EnterMarket = marketSystem.EnterMarket();
         if (EnterMarket != null)
@@ -73,6 +91,11 @@ public class Services {
     }
     //Make:nitay   IncorrectIdentification, InvalidSequenceOperationsExc
     //TODO: need to check
+
+    /**
+     * User closes the system
+     * @return - null
+     */
     public Response LeaveMarket() {
         try {
             User output = marketSystem.LeaveMarket("");
@@ -90,27 +113,29 @@ public class Services {
 
 
     //General Member-Visitor
+
+    /**
+     * User logs off
+     * @param username - user identifier
+     * @return Response object
+     */
     public ResponseT<PresentationUser> Logout(String username) {
         User output;
         try {
             output = marketSystem.logout(username);
             return new ResponseT<>(new PresentationUser(output));
-        } catch (BlankDataExc blankDataExc) {
+        } catch (BlankDataExc | InvalidSequenceOperationsExc | IncorrectIdentification blankDataExc) {
             return new ResponseT<>(null,blankDataExc.getLocalizedMessage());
-        } catch (InvalidSequenceOperationsExc invalidSequenceOperationsExc) {
-            return new ResponseT<>(null,invalidSequenceOperationsExc.getLocalizedMessage());
-        } catch (IncorrectIdentification incorrectIdentification) {
-            return new ResponseT<>(null,incorrectIdentification.getLocalizedMessage());
         }
     }
     //Make:nitay
 
     /***
      * Create a store object to represent the relevant store in the app
-     * @param description, description of the created shop
-     * @param username
-     * @param shopName
-     * @return
+     * @param description -  description of the created shop
+     * @param username - user identifier
+     * @param shopName - shop name
+     * @return Response object
      */
     public ResponseT<PresentationShop> CreateShop(String description ,String username, String shopName) {
         try {
@@ -118,10 +143,8 @@ public class Services {
             if (output != null)
                 return new ResponseT<>(new PresentationShop(output));
             else return new ResponseT<>();
-        } catch (BlankDataExc blankDataExc) {
+        } catch (BlankDataExc | IncorrectIdentification blankDataExc) {
             return new ResponseT<>(null,blankDataExc.getLocalizedMessage());
-        } catch (IncorrectIdentification incorrectIdentification) {
-            return new ResponseT<>(null,incorrectIdentification.getLocalizedMessage());
         }
     }
 
@@ -153,6 +176,14 @@ public class Services {
         return new Result<>(ans, ans);
     }
 
+    /**
+     * Boot of a user who started the system
+     * @param payment - External device for payment
+     * @param supply - External device for supply
+     * @param userName - user identifier
+     * @param password - user's given password
+     * @return Response object
+     */
     public Response StartMarket(PaymentService payment, SupplyService supply, String userName, String password) {
         try {
             boolean b = marketSystem.start(payment, supply, userName, password);
@@ -184,6 +215,13 @@ public class Services {
 
     //make: shahar
     //Guest-Visitor Shop options
+
+    /**
+     * Query for certain shops
+     * @param userName - user identifier
+     * @param filter - Identifying parameters
+     * @return list of Response object
+     */
     public List<ResponseT<PresentationShop>> GetShopsInfo(String userName, Filter<Shop> filter) {
         try {
             List<ResponseT<PresentationShop>> list = new LinkedList<>();
@@ -201,10 +239,10 @@ public class Services {
 
     /***
      * Query for certain products in the relevant store.
-     * @param userName
-     * @param shopID
+     * @param userName - user identifier
+     * @param shopID - shop identifier
      * @param f
-     * @return
+     * @return list of Response object
      */
     public List<ResponseT<PresentationProduct>> GetProductInfoInShop(String userName , int shopID, Filter<Product> f)
     {
@@ -228,10 +266,10 @@ public class Services {
 
     /**
      * Query for certain products by name in the relevant store
-     * @param userName
-     * @param pName
+     * @param userName - user identifier
+     * @param pName - shop's name
      * @param f
-     * @return
+     * @return list of Response object
      */
     public List<ResponseT<PresentationProduct>> SearchProductByName(String userName ,String pName, Filter<Product> f) //done
     {
@@ -255,7 +293,7 @@ public class Services {
      * @param userName
      * @param keyword
      * @param f
-     * @return
+     * @return list Response object
      */
     public List<ResponseT<PresentationProduct>> SearchProductByKeyword(String userName ,String keyword, Filter<Product> f)
     {
@@ -280,7 +318,7 @@ public class Services {
      * @param shopID - shop identifier
      * @param productId - product identifier
      * @param amount
-     * @return
+     * @return Response object
      */
     public Response AddToShoppingCart(String userName, int shopID,int productId, int amount)
     {
@@ -307,7 +345,7 @@ public class Services {
      * @param shopId - shop identifier
      * @param productId - product identifier
      * @param amount
-     * @return
+     * @return Response object
      */
     public Response EditShoppingCart(String userName, int shopId, int productId, int amount)
     {
@@ -326,6 +364,15 @@ public class Services {
     //make:shahar
 
     //TODO:
+
+    /**
+     * Remove a product from the user's shopping cart
+     * @param userName - user identifier
+     * @param shopId -  shop identifier
+     * @param productId - product identifier
+     * @return Response object
+     * @throws InvalidSequenceOperationsExc
+     */
     public Response RemoveFromShoppingCart(String userName, int shopId, int productId) throws InvalidSequenceOperationsExc {
         boolean success = marketSystem.removeProductFromCart(userName, shopId, productId);
         if(success)
@@ -334,7 +381,18 @@ public class Services {
             return new Response();
     }
 
-    //Make nitay  IncorrectIdentification, BlankDataExc
+    //Make nitay
+
+    /**
+     * Checkout
+     * @param userName - user identifier
+     * @param fullName - user full name
+     * @param address - user address
+     * @param phoneNumber - user phone number
+     * @param cardNumber - user credit card number
+     * @param expirationDate - user credit card expiration date
+     * @return list of Response object
+     */
     public List<Response> Checkout(String userName,String fullName, String address, String phoneNumber, String cardNumber, String expirationDate) {
         List<Response> checkout = new LinkedList<>();
         try {
@@ -356,6 +414,13 @@ public class Services {
 
 
     //shay
+
+    /**
+     * Check If Product Available in the shop
+     * @param p - relevant product object
+     * @param shopID - shop identifier
+     * @return Response object
+     */
     public ResponseT<Boolean> CheckIfProductAvailable(Product p, int shopID)
     {
         ShopController controller = ShopController.getInstance();
@@ -370,6 +435,18 @@ public class Services {
 
     //Shay
     //done
+
+    /**
+     * Add a product to the shop inventory
+     * @param pName - product name
+     * @param pDis - product description
+     * @param pCat - product category
+     * @param price - product price
+     * @param amount - amount of added product
+     * @param username - active user identifier
+     * @param shopID - shop identifier
+     * @return Response object
+     */
     public ResponseT<PresentationProduct> AddProductToShopInventory(String pName, String pDis, String pCat, double price, int amount, String username,int shopID)
     {
         ShopController controller = ShopController.getInstance();
@@ -401,7 +478,7 @@ public class Services {
      * @param username
      * @param p
      * @param shopID
-     * @return
+     * @return Response object
      */
     public ResponseT<PresentationProduct> ChangeProduct(String username, Product p, int shopID)
     {
@@ -434,7 +511,7 @@ public class Services {
      * @param key - shop identifier
      * @param targetUser - Passive user identifier
      * @param userName - Active user identifier
-     * @return
+     * @return Response object
      */
     public Response AppointNewShopOwner(int key,String targetUser, String userName)
     {
@@ -459,7 +536,7 @@ public class Services {
      * @param key - shop identifier
      * @param targetUser - Passive user identifier
      * @param userName - Active user identifier
-     * @return
+     * @return Response object
      */
     public Response AppointNewShopManager(int key,String targetUser, String userName) {
         try {
@@ -481,7 +558,7 @@ public class Services {
      * @param shopManagersPermissionsList - wanted permissions to add
      * @param targetUser - the manager who receives the permissions identifier.
      * @param ownerID - the user who gives  the permissions identifier.
-     * @return
+     * @return Response object
      */
     public Response AddShopMangerPermissions(int key, List<ShopManagersPermissions> shopManagersPermissionsList, String targetUser , String ownerID) {
         try {
@@ -505,7 +582,7 @@ public class Services {
      * @param shopManagersPermissionsList
      * @param managerUser
      * @param ownerID
-     * @return
+     * @return Response object
      */
     public Response RemoveShopManagerPermissions(int key, List<ShopManagersPermissions> shopManagersPermissionsList, String managerUser , String ownerID)
     {
@@ -529,7 +606,7 @@ public class Services {
      * Close an active shop
      * @param shopId
      * @param userName
-     * @return
+     * @return Response object
      */
     public Response CloseShop(int shopId,String userName) {
         try {
@@ -547,7 +624,7 @@ public class Services {
      * Make a closed shop -> active
      * @param shopId
      * @param userName
-     * @return
+     * @return Response object
      */
     public Response OpenShop(int shopId,String userName) {
         try {
@@ -564,10 +641,10 @@ public class Services {
 
     /**
      * Query for information about the relevant shop's employees
-     * @param shopName
+     * @param shopName - shop identifier
      * @param f
-     * @param userName
-     * @return
+     * @param userName - user identifier
+     * @return Response object
      */
     public List<ResponseT<PresentationUser>> RequestShopOfficialsInfo(int shopName, SearchOfficialsFilter f, String userName)
     {
@@ -588,10 +665,10 @@ public class Services {
 
     /**
      * Query for store purchase history
-     * @param shopName
+     * @param shopName - shop identifier
      * @param f
-     * @param userName
-     * @return
+     * @param userName - user identifier
+     * @return list of Response object
      */
     public List<ResponseT<Order>> RequestInformationOfShopsSalesHistory(int shopName, SearchOrderFilter f, String userName)
     {
@@ -640,7 +717,7 @@ public class Services {
      * @param productId
      * @param username
      * @param shopname
-     * @return
+     * @return Response object
      */
     public Response RemoveProductFromShopInventory(int productId, String username, int shopname)
     {
@@ -662,7 +739,7 @@ public class Services {
      * @param userName
      * @param f
      * @param shopID
-     * @return
+     * @return list of Response object
      */
     public List<ResponseT<Order>> getOrderHistoryForShops(String userName, Filter<Order> f, List<Integer> shopID){
         List<ResponseT<Order>> output = new LinkedList<>();
@@ -685,7 +762,7 @@ public class Services {
      * @param userName
      * @param f
      * @param userNames
-     * @return
+     * @return list of Response object
      */
     public List<ResponseT<Order>> getOrderHistoryForUser(String userName, Filter<Order> f, List<String>  userNames){
         List<ResponseT<Order>> output= new LinkedList<>();
@@ -695,12 +772,8 @@ public class Services {
             for(Order order:result)
                 output.add(new ResponseT<>(order));
         }
-        catch (InvalidAuthorizationException iae){
+        catch (InvalidAuthorizationException | IncorrectIdentification iae){
             output.add(new ResponseT(iae.getLocalizedMessage()));
-            return output;
-        }
-        catch (IncorrectIdentification incorrectIdentification){
-            output.add(new ResponseT(incorrectIdentification.getLocalizedMessage()));
             return output;
         }
         return output;
@@ -708,9 +781,9 @@ public class Services {
 
     /**
      * Remove registered user from the Market if it has no permissions
-     * @param usernames
-     * @param targetUser
-     * @return
+     * @param usernames - active user identifier
+     * @param targetUser - positive user identifier
+     * @return Response object
      */
     public Response DismissalUserBySystemManager(String usernames,String targetUser) {
         try {
@@ -718,23 +791,17 @@ public class Services {
                 return new Response();
             return new ResponseT(null,"");
         }
-        catch (BlankDataExc blankDataExc){
+        catch (BlankDataExc | IncorrectIdentification | InvalidSequenceOperationsExc blankDataExc){
             return new Response(blankDataExc.getLocalizedMessage());
-        }
-        catch (InvalidSequenceOperationsExc invalidSequenceOperationsExc){
-            return new Response(invalidSequenceOperationsExc.getLocalizedMessage());
-        }
-        catch ( IncorrectIdentification incorrectIdentification){
-            return new Response(incorrectIdentification.getLocalizedMessage());
         }
     }
 
     /**
      * Remove a store owner's permission in a particular store
-     * @param usernames
-     * @param targetUser
-     * @param shop
-     * @return
+     * @param usernames - active user identifier
+     * @param targetUser - positive user identifier
+     * @param shop - shop identifier
+     * @return Response object
      */
     public Response DismissalOwnerByOwner(String usernames,String targetUser,int shop) {
         try {
@@ -760,7 +827,7 @@ public class Services {
      * System manager query for user's Info
      * @param f
      * @param userName
-     * @return
+     * @return list of Response object
      */
     public List<ResponseT<PresentationUser>> RequestUserInfo(SearchUserFilter f, String userName)
     {
