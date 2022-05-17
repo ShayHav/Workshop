ad5cc0d5b8188ed03c93e10f4ada95ed911a6316
arielr77 <arielron@post.bgu.ac.il>
2022-05-17 15:37:37 +0300

    Stop

---
+++ code/src/Testing_System/AccepanceTests/CheckoutCaseTest.java
@@ -0,0 +1,15 @@
+package Testing_System.AccepanceTests;
+
+import Testing_System.Tester;
+import org.junit.jupiter.api.Test;
+
+
+/* https://github.com/ShayHav/Workshop/wiki/Use-Cases */
+
+public class CheckoutCaseTest extends Tester {
+
+
+
+    @Test
+    public void GoodCheckOut()
+}
---
+++ code/src/Testing_System/AccepanceTests/OpenShopCaseTest.java
@@ -0,0 +1,7 @@
+package Testing_System.AccepanceTests;
+
+import Testing_System.Tester;
+
+/* https://github.com/ShayHav/Workshop/wiki/Use-Cases */
+public class OpenShopCaseTest extends Tester {
+}
