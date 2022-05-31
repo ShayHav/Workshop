5ff46b30d6380a2b413a0bb68ea2ec18295b9321
Shay <shayhav@post.bgu.ac.il>
2022-05-31 15:38:01 +0300

    add feature to mark messages as read on all pages

--- code/src/main/java/Presentation/Model/Messages/NotificationMessage.java
+++ code/src/main/java/Presentation/Model/Messages/NotificationMessage.java
@@ -2,12 +2,16 @@ package Presentation.Model.Messages;
 
 import domain.notifications.Message;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class NotificationMessage {
 
     private String sender;
     private String addressee;
     private String content;
     private boolean read;
+    private LocalDateTime sentDate;
 
     public NotificationMessage(){}
 
@@ -16,6 +20,7 @@ public class NotificationMessage {
         this.addressee = domainMessage.getAddressee().getUserName();
         this.content = domainMessage.getContent();
         this.read = domainMessage.isRead();
+        sentDate = domainMessage.getSentDate();
     }
 
     public String getContent() {
@@ -48,5 +53,26 @@ public class NotificationMessage {
 
     public void setAddressee(String addressee) {
         this.addressee = addressee;
+    }
+
+    public LocalDateTime getSentDate() {
+        return sentDate;
+    }
+
+    public void setSentDate(LocalDateTime sentDate) {
+        this.sentDate = sentDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationMessage message = (NotificationMessage) o;
+        return Objects.equals(sender, message.sender) && Objects.equals(addressee, message.addressee) && Objects.equals(content, message.content) && Objects.equals(sentDate, message.sentDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sender, addressee, content, sentDate);
     }
 }
