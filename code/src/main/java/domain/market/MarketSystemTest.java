eccd9e2ce3966503ae5e1ea848c25b6a2563aa99
nitay vitkin <nitayv@post.bgu.ac.il>
2022-05-31 19:59:03 +0300

    fix dismissOwner

--- code/src/main/java/domain/market/MarketSystemTest.java
+++ code/src/main/java/domain/market/MarketSystemTest.java
@@ -293,7 +293,68 @@ class MarketSystemTest {
     }
 
     @Test
-    void logout() {
+    void checkOut() throws ShopNotFoundException, InvalidSequenceOperationsExc, IncorrectIdentification, BlankDataExc {
+        List<ShopManagersPermissions> shopManagersPermissionsList = new LinkedList<>();
+        shopManagersPermissionsList.add(ShopManagersPermissions.OpenShop);
+        shopManagersPermissionsList.add(ShopManagersPermissions.CloseShop);
+        PaymentService ps1 = mock(PaymentService.class);
+        when(ps1.connect()).thenReturn(true);
+        SupplyService ss2 = mock(SupplyService.class);
+        when(ss2.connect()).thenReturn(true);
+        marketSystem.start(ps1,ss2,"admin","admin");
+        marketSystem.register(userName[0],userPass[0] );
+        DiscountPolicy discountPolicy = mock(DiscountPolicy.class);
+        PurchasePolicy purchasePolicy = mock(PurchasePolicy.class);
+        ExecutorService pool = Executors.newFixedThreadPool(nitayName.length);
+        for(int i =0;i<4;i++){
+            int finalI = i;
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        marketSystem.register(nitayName[finalI], nitayPass[finalI]);
+                        marketSystem.register(userName[finalI], userPass[finalI]);
+                        marketSystem.logIn(nitayName[finalI], nitayPass[finalI],null);
+                        Shop s = marketSystem.createShop("sports",nitayName[finalI] + "'s", discountPolicy, purchasePolicy, nitayName[finalI]);
+                        marketSystem.AppointNewShopManager(s.getShopID(),userName[finalI],nitayName[finalI]);
+                        marketSystem.AddShopMangerPermissions(s.getShopID(),shopManagersPermissionsList,userName[finalI],nitayName[finalI]);
+                        marketSystem.CloseShop(s.getShopID(),userName[finalI]);
+                        marketSystem.OpenShop(s.getShopID(),userName[finalI]);
+                        marketSystem.AddProductToShopInventory(001,"basketball","sports","sports",50.0,50,nitayName[finalI],s.getShopID());
+                        marketSystem.logout(nitayName[finalI]);
+                        marketSystem.logIn(userName[finalI], userPass[finalI],null);
+                        marketSystem.AddProductToCart(userName[finalI],s.getShopID(),001,1);
+                        marketSystem.Checkout(userName[finalI],userName[finalI],"Beer shava","+972528403121","0123456789","07/26");
+                    } catch (InvalidSequenceOperationsExc e) {
+                        e.printStackTrace();
+                    } catch (IncorrectIdentification incorrectIdentification) {
+                        incorrectIdentification.printStackTrace();
+                    } catch (InvalidAuthorizationException e) {
+                        e.printStackTrace();
+                    } catch (BlankDataExc blankDataExc) {
+                        blankDataExc.printStackTrace();
+                    } catch (ShopNotFoundException shopNotFoundException) {
+                        shopNotFoundException.printStackTrace();
+                    }
+                }
+            };
+            pool.execute(r);
+        }
+
+
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+        for(int i =1;i<4;i++){
+            Shop s = marketSystem.getShop(i);
+            assertTrue(s.isOpen());
+        }
     }
 
     @Test
