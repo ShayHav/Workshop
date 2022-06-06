7093619f1e0d788b7b1d4b2a6991dad87fdc7fa3
shaharlankry <62591554+shaharlankry@users.noreply.github.com>
2022-06-06 15:43:00 +0300

    merged presentation-cont into main and refactored classes

--- code/tests/domain/user/SecurePasswordStorageTest.java
+++ code/tests/domain/user/SecurePasswordStorageTest.java
@@ -1,6 +1,7 @@
 package domain.user;
 
 import Testing_System.UserGenerator;
+import domain.Exceptions.InvalidSequenceOperationsExc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,7 @@ class SecurePasswordStorageTest {
 
     @BeforeEach
     void setUp(){
-        securePasswordStorage = new SecurePasswordStorage();
+        securePasswordStorage = SecurePasswordStorage.getSecurePasswordStorage_singleton();
         for(int i = 0;i<userName.length;i++){
             securePasswordStorage.inRole(userName[i],userPass[i]);
         }
@@ -34,4 +35,26 @@ class SecurePasswordStorageTest {
             assertTrue(securePasswordStorage.isUserRole(userName[i]));
         }
     }
+
+    @Test
+    void passwordCheckThread() throws InterruptedException {
+        Thread t0 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
+                securePasswordStorage.passwordCheck(userName[0],userPass[0]);
+
+            }
+        });
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+                securePasswordStorage.passwordCheck(userName[1],userPass[1]);
+            }
+        });
+        t0.start();
+        t1.start();
+    }
+
 }
--- code/tests/domain/user/ShoppingBasketTest.java
+++ code/tests/domain/user/ShoppingBasketTest.java
@@ -1,87 +1,133 @@
 package domain.user;
 
-import domain.shop.ProductInfo;
-import domain.Exceptions.ProductNotFoundException;
-import domain.shop.Shop;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.Map;
-
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import domain.Exceptions.*;
+import domain.market.MarketSystem;
+import domain.market.PaymentServiceImp;
+import domain.market.SupplyServiceImp;
+import domain.shop.ProductInfo;
+import domain.shop.PurchasePolicys.PurchasePolicy;
+import domain.shop.Shop;
+import domain.shop.ShopController;
+import domain.shop.discount.DiscountPolicy;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.rmi.server.ServerCloneException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class ShoppingBasketTest {
 
+
     ShoppingBasket basket;
+    User user;
     Shop shop;
-    ProductInfo productInfo;
+    MarketSystem marketSystem = MarketSystem.getInstance();
+    UserController userController = UserController.getInstance();
+    ShopController shopController = ShopController.getInstance();
 
-    @BeforeEach
-    void setUp() {
-        shop = mock(Shop.class);
-        productInfo = mock(ProductInfo.class);
-        basket = new ShoppingBasket(shop);
-        Map<Integer, Integer> productsWithAmount = basket.getProductAmountList();
-        productsWithAmount.put(1, 100);
-    }
 
 
     @Test
-    void addProductToBasket() {
-        assertFalse(basket.addProductToBasket(5, -1));
-        assertTrue(basket.addProductToBasket(5, 0));
-        assertTrue(basket.getProductAmountList().containsKey(5));
-        assertTrue(basket.addProductToBasket(5, 50));
-        assertNotEquals(basket.getProductAmountList().get(1),50);
-        assertTrue(basket.addProductToBasket(1, 50));
-        assertEquals(basket.getProductAmountList().get(1), 150);
-
+    void addProductToBasket() throws IncorrectIdentification, InvalidSequenceOperationsExc, BlankDataExc, InvalidAuthorizationException, ShopNotFoundException {
+        marketSystem.start(new PaymentServiceImp(),new SupplyServiceImp(),"admin","admin");
+        marketSystem.register("nitay","Password");
+        marketSystem.logIn("nitay","Password",null);
+        marketSystem.createShop("book shop","nitay's",new DiscountPolicy(),new PurchasePolicy(),"nitay");
+        marketSystem.AddProductToShopInventory(1,"Dune","Best Thing ever","books",90.0,50,"nitay",1);
+        basket = new ShoppingBasket(marketSystem.getShop(1));
+        try{
+            basket.addProductToBasket(5, -1);
+        }
+        catch (IllegalArgumentException illegalArgumentException){
+            assertTrue(false);
+        }
+        catch (ProductNotFoundException productNotFoundException) {
+            assertTrue(true);
+        }
+        try {
+            basket.addProductToBasket(5, 0);
+        }
+        catch (IllegalArgumentException illegalArgumentException){
+            assertTrue(false);
+        }
+        catch (ProductNotFoundException productNotFoundException) {
+            assertTrue(true);
+        }
+        assertFalse(basket.getProductAmountList().containsKey(5));
     }
 
     @Test
-    void calculateTotalAmount() {
+    void calculateTotalAmount() throws InvalidSequenceOperationsExc, IncorrectIdentification, ShopNotFoundException, BlankDataExc, InvalidAuthorizationException {
+        marketSystem.start(new PaymentServiceImp(),new SupplyServiceImp(),"admin","admin");
+        marketSystem.register("nitay","Password");
+        marketSystem.logIn("nitay","Password",null);
+        marketSystem.createShop("book shop","nitay's",new DiscountPolicy(),new PurchasePolicy(),"nitay");
+        marketSystem.AddProductToShopInventory(1,"Dune","Best Thing ever","books",90.0,50,"nitay",1);
+        basket = new ShoppingBasket(marketSystem.getShop(1));
         Map<Integer,Integer> productsWithAmount = basket.getProductAmountList();
-        when(shop.calculateTotalAmountOfOrder(productsWithAmount)).thenReturn(100.0);
-        assertEquals(basket.calculateTotalAmount(), 100.0);
-        productsWithAmount.put(2, 50);
-        when(shop.calculateTotalAmountOfOrder(productsWithAmount)).thenReturn(200.0);
-        assertEquals(basket.calculateTotalAmount(), 200.0);
+        assertEquals(basket.calculateTotalAmount(), 0);
+        productsWithAmount.put(1, 2);
+        assertEquals(basket.calculateTotalAmount(), 180.0);
     }
 
     @Test
-    void updateAmount() {
-        assertFalse(basket.updateAmount(1, -1));
+    void updateAmount() throws ProductNotFoundException, InvalidSequenceOperationsExc, IncorrectIdentification, InvalidAuthorizationException, BlankDataExc, ShopNotFoundException {
+        marketSystem.start(new PaymentServiceImp(),new SupplyServiceImp(),"admin","admin");
+        marketSystem.register("nitay","Password");
+        marketSystem.logIn("nitay","Password",null);
+        marketSystem.createShop("book shop","nitay's",new DiscountPolicy(),new PurchasePolicy(),"nitay");
+        marketSystem.AddProductToShopInventory(1,"Dune","Best Thing ever","books",90.0,50,"nitay",1);
+        basket = new ShoppingBasket(marketSystem.getShop(1));
+        basket.addProductToBasket(1,1);
+        try {
+            basket.updateAmount(1, -1);
+        }
+        catch (IllegalArgumentException illegalArgumentException){
+            assertTrue(true);
+        }
         assertTrue(basket.getProductAmountList().containsKey(1));
-        assertTrue(basket.updateAmount(1, 50));
+        basket.updateAmount(1, 20);
         assertTrue(basket.getProductAmountList().containsKey(1));
-        assertTrue(basket.updateAmount(1, 0));
-        assertFalse(basket.getProductAmountList().containsKey(1));
-    }
-
-    @Test
-    void removeProduct() {
-        basket.getProductAmountList().put(2,200);
-        assertTrue(basket.removeProduct(1));
-        assertFalse(basket.getProductAmountList().containsKey(1));
-        assertTrue(basket.removeProduct(2));
+        assertTrue(basket.getProductAmountList().get(1).equals(20));
         assertFalse(basket.getProductAmountList().containsKey(2));
     }
 
+    @Test
+    void removeProduct() throws ProductNotFoundException, InvalidSequenceOperationsExc, IncorrectIdentification, InvalidAuthorizationException, BlankDataExc, ShopNotFoundException {
+        marketSystem.start(new PaymentServiceImp(),new SupplyServiceImp(),"admin","admin");
+        marketSystem.register("nitay","Password");
+        marketSystem.logIn("nitay","Password",null);
+        marketSystem.createShop("book shop","nitay's",new DiscountPolicy(),new PurchasePolicy(),"nitay");
+        marketSystem.AddProductToShopInventory(1,"Dune","Best Thing ever","books",90.0,50,"nitay",1);
+        basket = new ShoppingBasket(marketSystem.getShop(1));
+        basket.addProductToBasket(1,1);
+        basket.removeProduct(1);
+        assertFalse(basket.getProductAmountList().containsKey(1));
+    }
+
 
     @Test
-    void showBasket() throws ProductNotFoundException {
-        when(shop.getShopID()).thenReturn(1);
-        when(shop.getName()).thenReturn("david&sons");
-        when(shop.getInfoOnProduct(1)).thenReturn(productInfo);
-        when(shop.calculateTotalAmountOfOrder(basket.getProductAmountList())).thenReturn(100.0);
-        ShoppingBasket.BasketInfo info = basket.showBasket();
-        assertEquals(info.getShopId(),1);
-        assertEquals(info.getShopName(),"david&sons");
-        assertEquals(info.getProductWithAmount().get(productInfo), 100);
-        assertEquals(info.getTotalAmount(), 100.0);
+    void showBasket() throws ProductNotFoundException, InvalidSequenceOperationsExc, IncorrectIdentification, InvalidAuthorizationException, BlankDataExc, ShopNotFoundException {
+        marketSystem.start(new PaymentServiceImp(),new SupplyServiceImp(),"admin","admin");
+        marketSystem.register("nitay","Password");
+        marketSystem.logIn("nitay","Password",null);
+        marketSystem.createShop("book shop","nitay's",new DiscountPolicy(),new PurchasePolicy(),"nitay");
+        marketSystem.AddProductToShopInventory(1,"Dune","Best Thing ever","books",90.0,50,"nitay",1);
+        basket = new ShoppingBasket(marketSystem.getShop(1));
+        basket.addProductToBasket(1,1);
+        ShoppingBasket.ServiceBasket b = basket.showBasket();
+        assertTrue(b.getShopId()==1);
+        assertTrue(b.getShopName().equals("nitay's"));
+        assertTrue(b.getProductWithAmount().size()==1);
+        assertFalse(b.getTotalAmount()==100);
     }
 }
